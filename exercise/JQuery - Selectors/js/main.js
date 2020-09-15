15a71d72fc6cb81211e6ae71c457a79604eb89f4
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-09-15 00:56:50 -0300

    JQuery: Add, Remove and Toggle Class

--- exercise/JQuery - Selectors/js/main.js
+++ exercise/JQuery - Selectors/js/main.js
@@ -1,9 +1,13 @@
 $(function(){
     var blueItems = $('.item-azul');
     var redItems = $('.item-vermelho');
+    var items = $('h1');
 
     var btnBlue = $('#faca-azul');
     var btnRed = $('#faca-vermelho');
+    var btnChange = $('.btn-change');
+    var btnRemove = $('.btn-remove');
+    var btnToggle = $('.btn-toggle');
 
     btnBlue.click(function(){
         blueItems.css({
@@ -19,5 +23,17 @@ $(function(){
             backgroundColor: 'white'
         })
         $('body').css('background-color','red');
+    })
+
+    btnChange.click(function(){
+        items.addClass('change-items');
+    })
+
+    btnRemove.click(function(){
+        items.removeClass('change-items');
+    })
+
+    btnToggle.click(function(){
+        items.toggleClass('change-items');
     })
 })
