12485f6512de1e9e9ba8e4987a05cbae330e8382
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-09-14 23:35:27 -0300

    JQuery: cat exercise part 01

---
+++ exercise/JQuery - Cat Exercise/js/main.js
@@ -0,0 +1,43 @@
+meusGatos = {
+    gato: [
+        {
+            "id": 1,
+            "nome": "Chico",
+            "descricao": "O Chico cansou de ser Gato e virou Dinossauro.",
+            "fotoURL": "images/gato-01.jpg",
+            "linkExterno": "https://www.instagram.com/canseidesergato/"
+        },
+
+        {
+            "id": 2,
+            "nome": "Hamilton",
+            "descricao": "Hamilton é o gato mais hipster que você vai ver hoje.",
+            "fotoURL": "images/gato-02.jpg",
+            "linkExterno": "https://www.instagram.com/hamilton_the_hipster_cat/"
+        },
+
+        {
+            "id": 3,
+            "nome": "Nala",
+            "descricao": "Nala é uma gatinha muito simpática e extrovertida.",
+            "fotoURL": "images/gato-03.jpg",
+            "linkExterno": "https://www.instagram.com/nala_cat/"
+        },
+    ]
+}
+
+//Fazemos um loop para checar se existe um gato com este nome 
+
+//Pegamos o valor do Input Field    
+
+//Rodamos a Função getGato ao clicar no botao
+
+$(document).ready(function(){
+    var cardGatoJquery = $('#card');
+    var btnJquery = $('#btn');
+
+    function getCat(){
+        var inputCat = $('#myInput').val().toLowerCase();
+    }
+})
+
